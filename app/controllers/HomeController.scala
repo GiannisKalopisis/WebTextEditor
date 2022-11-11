93810f0ad603f5c7d83b511f69a47c8d21fe5fdf
Giannis Kalopisis <gianniskalopisis@gmail.com>
2022-11-11 10:46:40 +0200

    Display files from memory

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import models.{FileModelInMemory}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,15 +11,54 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+	
+	/**
+	 * Create an Action to render an HTML page.
+	 *
+	 * The configuration in the `routes` file means that this method
+	 * will be called when the application receives a `GET` request with
+	 * a path of `/`.
+	 */
+	
+	
+	/**
+	 * request is a function that is the request to the result that we want
+	 *
+	 * It has additional information, including the body
+	 */
+	
+	def viewAllFilesTitle(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		val files = FileModelInMemory.getAllTitles
+		Ok(views.html.viewAllFilesTitle(files))
+	}
+	
+	def getText(title : String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		val text = FileModelInMemory.getFile(title)
+		Ok(views.html.fileView(title, text))
+	}
+	
+	/**
+	 * No need for parameters (they aren't in URL), because its a POST method.
+	 *
+	 * We will take them from body
+	 */
+	//	def printFilePost: Action[AnyContent] = Action { request =>
+	//		val postVals = request.body.asFormUrlEncoded
+	//		postVals.map { args =>
+	//			val id = args("id").head
+	//			val name = args("name").head
+	//			val text = args("text").head
+	//			Ok(s"File '$name', with id '$id' and text '$text'")
+	//		}.getOrElse(Ok(""))
+	//	}
+	
+	def printTestFile(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		//		val file = TestFile(1, "file1", "This is dummy text")
+		Ok(views.html.index())
+	}
+	
+	def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		Ok(views.html.index())
+	}
+	
 }
--- app/models/FileModelInMemory.scala
+++ app/models/FileModelInMemory.scala
@@ -1,5 +1,38 @@
 package models
 
-object FileModelInMemory {
+import scala.collection.mutable
 
+object FileModelInMemory {
+	
+	private val files = mutable.Map[String, String](
+		"java" -> "Dummy text for file 1",
+		"c++" -> "Dummy text for file 2",
+		"python" -> "Dummy text for file 3",
+		"scala" -> "Dummy text for file 4"
+	)
+	
+	private def fileExists(title : String): Boolean = {
+		if (files.contains(title)) true else false
+	}
+	
+	def getAllTitles: Seq[String] = {
+		files.keySet.toSeq
+	}
+	
+	def getFile(title: String): String = {
+		files(title)
+	}
+	
+	def update(title: String, text: String): Unit = {
+		if (fileExists(title)) {
+			files(title) = text
+		}
+		else {
+			addFile(title, text)
+		}
+	}
+	
+	def addFile(title: String, text: String): Unit = {
+		files.addOne(title, text)
+	}
 }
